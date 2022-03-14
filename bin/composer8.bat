cba9e3119ecab264d2b24b71909ebcd9c274e6b8
Oskar Królikowski <oskar.krolikowski@cyfrowafoto.com>
2022-03-14 11:22:15 +0100

    php 8.1.3 as default php8

--- bin/composer8.bat
+++ bin/composer8.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-CALL "%~dp0..\php\8.1.1\php.exe" "%~dp0..\composer\composer.phar" %*
+CALL "%~dp0..\php\8.1.3\php.exe" "%~dp0..\composer\composer.phar" %*
--- bin/php8.bat
+++ bin/php8.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-CALL "%~dp0..\php\8.1.1\php.exe" %*
+CALL "%~dp0..\php\8.1.3\php.exe" %*
